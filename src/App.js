0e9e9e8a3ca64e52c7d1636105d561c8cb90b56a
Alexandr <gayrsuk@gmail.com>
2021-07-22 23:28:25 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { ErrorBoundary } from './components';
+import { AppRouter, Routes } from './routes';
+import { AppStore } from './store';
+import { AppThemeProvider } from './theme';
+import { SnackBarProvider } from './components/SnackBar';
+import IdleTimer from './components/IdleTimer';
+
+/**
+ * Root Application Component
+ * @class App
+ */
+const App = () => {
+  return (
+    <ErrorBoundary name="App">
+      <AppStore>
+        <IdleTimer />
+        <AppThemeProvider>
+          <SnackBarProvider>
+            <AppRouter>
+              <Routes />
+            </AppRouter>
+          </SnackBarProvider>
+        </AppThemeProvider>
+      </AppStore>
+    </ErrorBoundary>
+  );
+};
+
+export default App;
---
+++ src/components/NotImplemented.js
@@ -0,0 +1,7 @@
+/**
+ * Renders "Component is under construction" boilerplate
+ * @class ComponentIsNotImplemented
+ */
+const ComponentIsNotImplemented = () => <div>Component is under construction</div>;
+
+export default ComponentIsNotImplemented;
